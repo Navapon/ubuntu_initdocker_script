85c10be2dc201859f716d33832cb572c245a25e3
John snow <navapon.dev@gmail.com>
2018-10-04 14:03:44 +0700

    updated docker-compose version

--- initial.sh
+++ initial.sh
@@ -28,7 +28,7 @@ sudo systemctl status docker
 
 ################ Install docker-compose ################
 # Download a package docker-compose from official Repo to /usr/local/bin/docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 # Set Permission to Docker-compose 
 sudo chmod +x /usr/local/bin/docker-compose
 # Check version for make sure installed

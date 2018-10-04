be3b0a8c30fd65708e3ff814af33e8df240daa3d
John snow <navapon.dev@gmail.com>
2018-10-04 14:28:56 +0700

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+#tuning sysctl.conf
+echo "net.core.rmem_max = 16777216" >> /etc/sysctl.conf
+echo "net.core.wmem_max = 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_rmem = 4096 87380 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_wmem = 4096 65536 16777216" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_probes = 9" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_keepalive_time = 7200" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syn_retries = 5" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_no_metrics_save = 1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_synack_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_syn_retries = 2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_tw_reuse=1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_fin_timeout=15" >> /etc/sysctl.conf
+echo "net.ipv4.ip_local_port_range = 2000 65000" >> /etc/sysctl.conf
+echo "fs.file-max = 1000000" >> /etc/sysctl.conf
+echo "vm.swappiness = 0" >> /etc/sysctl.conf
+echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
+
 # Create user 1001 with uid = 1001
 sudo useradd -u 1001 --no-create-home 1001
 
@@ -30,6 +48,7 @@ sudo systemctl enable docker
 #Add ubuntu and 1001 to docker group
 sudo usermod -a -G docker ubuntu
 sudo usermod -a -G docker 1001
+sudo usermod -a -G 1001 ubuntu
 
 ################ Install docker-compose ################
 # Download a package docker-compose from official Repo to /usr/local/bin/docker-compose
